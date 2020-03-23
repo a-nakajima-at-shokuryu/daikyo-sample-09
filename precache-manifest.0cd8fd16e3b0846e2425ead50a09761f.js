b6f02e2801946afa7a98385f1f17ba68b29ad40e
中島　亮三 <aki323buri2@gmail.com>
2020-03-23 13:04:37 +0900

    Updates

---
+++ precache-manifest.0cd8fd16e3b0846e2425ead50a09761f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf182f7c05984d13032fdfa547e1ef2",
+    "url": "/daikyo-sample-09/index.html"
+  },
+  {
+    "revision": "e0cdd98c1cacd2709c58",
+    "url": "/daikyo-sample-09/static/js/2.0024d3ce.chunk.js"
+  },
+  {
+    "revision": "aa2d0aa3f566e2a6891602b21f6d239e",
+    "url": "/daikyo-sample-09/static/js/2.0024d3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2cb1e5940c9fd2c3cf",
+    "url": "/daikyo-sample-09/static/js/main.c30d8c58.chunk.js"
+  },
+  {
+    "revision": "10e55023b93e86d0c41a",
+    "url": "/daikyo-sample-09/static/js/runtime-main.bc06b4a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b680c9706859f9f2f48efe97b6f81a3.js"
+  "/daikyo-sample-09/precache-manifest.0cd8fd16e3b0846e2425ead50a09761f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daikyo-sample-09/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
